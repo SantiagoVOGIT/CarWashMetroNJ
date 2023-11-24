4943084c3ba3e598b8f8657b83b0e932be44e2fe
Santiago Valencia Ochoa <149266824+SantiagoVOGIT@users.noreply.github.com>
2023-11-24 12:15:12 -0500

    Configuración de login de admin
    - Se configuro el login para que usuarios y admins puedan acceder
    
    - Se configuraron las rutas de admin
    
    - Se agrego el model de admin básico

---
+++ models/adminsModel.js
@@ -0,0 +1,17 @@
+const db = require("../database");
+
+const AdminsModel = {
+  createAdmin: (admin, callback) => {
+    db.query("INSERT INTO admins SET ?", admin, callback);
+  },
+
+  getAdminByCorreoContrasena: (correo, contrasena, callback) => {
+    db.query(
+      "SELECT * FROM admins WHERE correo = ? AND contrasena = ?",
+      [correo, contrasena],
+      callback
+    );
+  },
+};
+
+module.exports = AdminsModel;
--- router/routesAdmin.js
+++ router/routesAdmin.js
@@ -1,16 +1,61 @@
 const express = require("express");
 const router = express.Router();
+const AdminsModel = require("../models/adminsModel");
+
+router.post("/", (req, res) => {
+  const { correo, contrasena } = req.body;
+
+  AdminsModel.getAdminByCorreoContrasena(
+    correo,
+    contrasena,
+    (error, results) => {
+      if (error) {
+        console.log(error);
+        res.render("index.ejs", {
+          message: { type: "error", text: "Error al iniciar sesión" },
+        });
+      } else {
+        if (results.length > 0) {
+          const admin = results[0];
+
+          // Si es un administrador, establecer la sesión y redirigir a dashboard
+          req.session.admin = admin;
+          res.redirect("/admin/dashboard");
+        } else {
+          res.render("index.ejs", {
+            message: {
+              type: "error",
+              text: "Correo o contraseña incorrectos",
+            },
+          });
+        }
+      }
+    }
+  );
+});
 
 router.get("/dashboard", (req, res) => {
-  res.render("admin/dashboard.ejs");
+  if (req.session.admin) {
+    res.render("admin/dashboard.ejs");
+  } else {
+    res.redirect("/");
+  }
 });
 
 router.get("/settings", (req, res) => {
-  res.render("admin/settings.ejs");
+  if (req.session.admin) {
+    res.render("admin/settings.ejs");
+  } else {
+    res.redirect("/");
+  }
 });
 
 router.get("/staff", (req, res) => {
-  res.render("admin/staff.ejs");
+  if (req.session.admin) {
+    res.render("admin/staff.ejs");
+  } else {
+    res.redirect("/");
+  }
 });
 
 module.exports = router;
